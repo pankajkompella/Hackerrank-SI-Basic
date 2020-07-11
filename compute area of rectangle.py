67c38d5171a3acaf3ade27744cbee4b2b8666005
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 13:33:15 +0530

    Create compute area of rectangle.py

---
+++ compute area of rectangle.py
@@ -0,0 +1,28 @@
+'''
+Given the length and breadth of the rectangle. Print area of the rectangle.
+
+Input Format
+
+Input contains two positive integers L - length of the rectangle and B - breadth of the rectangle.
+
+Constraints
+
+1 <= L, B <=109
+
+Output Format
+
+Print area of the rectangle.
+
+Sample Input 0
+
+5 8
+Sample Output 0
+
+40
+Explanation 0
+
+Self Explanatory
+'''
+
+l,b=map(int,input().split(' '))
+print(l*b)
