f5d8ef4129e4211db6524dcb063b8e4a126d7903
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 13:10:51 +0530

    Update print unique elements of array.py

--- print unique elements of array.py
+++ print unique elements of array.py
@@ -1,3 +1,28 @@
+'''
+Print unique elements of the array in the same order as they appear in the input.
+Note: Do not use any inbuilt functions/libraries for your main logic.
+
+Input Format:
+First line of input contains a single integer N - the size of array and second line contains array elements.
+
+Constraints:
+1 <= N <= 100
+0 <= ar[i] <= 109
+
+Output Format:
+Print unique elements of the array.
+
+Sample Input 0:
+7
+5 4 10 9 21 4 10
+
+Sample Output 0:
+5 9 21
+
+Explanation 0:
+Self Explanatory.
+'''
+
 n=int(input())
 c=[]
 a=list(map(int,input().split()))
