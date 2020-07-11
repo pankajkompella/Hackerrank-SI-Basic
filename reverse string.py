b59bb827e7fde853be2addb6fcf064ec38cb8892
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 14:10:43 +0530

    Create reverse string.py

---
+++ reverse string.py
@@ -0,0 +1,28 @@
+'''
+Given a string, reverse the given string.
+
+Input Format
+
+Input contains a String - S.
+
+Constraints
+
+1 <= len(s) <= 100
+
+Output Format
+
+Print the reversed string.
+
+Sample Input 0
+
+fdsrd
+Sample Output 0
+
+drsdf
+Explanation 0
+
+Self Explanatory
+'''
+
+s=str(input())
+print(s[::-1])
