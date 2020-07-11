6215238bc5421b5e05e9806129f9d6328c761b8e
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 14:11:33 +0530

    Create toggle case of characters.py

---
+++ toggle case of characters.py
@@ -0,0 +1,28 @@
+'''
+Given a string, toggle case of each character in the given string.
+
+Input Format
+
+Input contains a string - S.
+
+Constraints
+
+1 <= len(S) <= 100
+
+Output Format
+
+Print the toggled string.
+
+Sample Input 0
+
+abdBd
+Sample Output 0
+
+ABDbD
+Explanation 0
+
+Self Explanatory
+'''
+
+s=str(input())
+print(s.swapcase())
