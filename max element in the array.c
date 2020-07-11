07f5cd522a48be2598702c5eff892766980c9e00
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 13:13:36 +0530

    Update max element in the array.c

--- max element in the array.c
+++ max element in the array.c
@@ -1,3 +1,23 @@
+/**
+Find maximum element from the given array of integers.
+
+Input Format:
+First line of input contains N - the size of the array and second line contains the elements of the array.
+Constraints:
+1 <= N <= 103
+-109 <= ar[i] <= 109
+Output Format:
+Print the maximum element of array.
+
+Sample Input 0:
+5
+-2 -19 8 15 4
+Sample Output 0:
+15
+Explanation 0:
+Self Explanatory
+**/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
