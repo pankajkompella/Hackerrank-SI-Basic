7860d802d2a195ca7febb929b6819cf843eed763
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 13:55:14 +0530

    Create right-angled triangle pattern 2.py

---
+++ right-angled triangle pattern 2.py
@@ -0,0 +1,32 @@
+#YET TO BE SOLVED
+
+'''
+Print right-angled triangle pattern. See example for more details.
+
+Input Format
+
+First line of input contains a single integer N - the size of the triangle.
+
+Constraints
+
+1 <= N <= 50
+
+Output Format
+
+For the given integer, print the right-angled triangle pattern.
+
+Sample Input 0
+
+5
+Sample Output 0
+
+1
+2 6
+3 7 10
+4 8 11 13
+5 9 12 14 15
+Explanation 0
+
+Self Explanatory
+'''
+
