8f5fa39f24fabf58126e5f5201557799e2b769af
S S Pankaj Kompella <52881759+pankajkompella@users.noreply.github.com>
2020-07-11 14:09:11 +0530

    Create prefix and suffix.py

---
+++ prefix and suffix.py
@@ -0,0 +1,28 @@
+#YET TO BE DONE
+
+'''
+Given a string, compute the length of longest prefix string which is same as the suffix of the string, the length of the prefix or suffix string must be less than the given input string.
+
+Input Format
+
+Input contains a string - S.
+
+Constraints
+
+1 <= len(S) <= 100
+
+Output Format
+
+Print length of longest prefix string which is same as suffix of the string.
+
+Sample Input 0
+
+smartintsmart
+Sample Output 0
+
+5
+Explanation 0
+
+Self Explanatory
+'''
+
