837ba98c31adafae4244448162527583e7a43ce8
tanu-shiv <114908699+tanu-shiv@users.noreply.github.com>
2023-01-08 23:26:21 +0530

    Update right-angled triangle pattern 2.py

--- right-angled triangle pattern 2.py
+++ right-angled triangle pattern 2.py
@@ -30,3 +30,28 @@ Explanation 0
 Self Explanatory
 '''
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    int N,k,m;
+    scanf("%d",&N);
+    if(N==1){
+        printf("1\n");
+    }
+    else{
+        for(int i=1;i<=N;i++){
+            m=N-1;
+            k=i;
+            for(int j=1;j<=i;j++){
+                printf("%d ",k);
+                k=k+m;
+                m--;
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
