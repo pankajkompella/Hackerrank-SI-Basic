cc368b9974ce87b67cb7ddae6b6c01017c3a2445
Chakradhar Cholleti <cholletichakradhar@gmail.com>
2023-03-23 18:52:20 +0530

    Update Right angled triangle pattern 2
    Added the solution to the problem

--- right-angled triangle pattern 2.py
+++ right-angled triangle pattern 2.py
@@ -25,7 +25,31 @@ Sample Output 0
 3 7 10
 4 8 11 13
 5 9 12 14 15
-Explanation 0
+Explanation:
+In java 8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        for(int i=1;i<=n;i++){
+            int k=0;
+            int l=n-1;
+            for(int j=1;j<=i;j++){
+                int s = i+k;
+                System.out.print(s+" ");
+                k+=l--;
+            }
+            System.out.println();
+        }
+    }
+}
 
 Self Explanatory
 '''
